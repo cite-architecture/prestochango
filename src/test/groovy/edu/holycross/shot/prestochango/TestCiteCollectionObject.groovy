5308d5f85924cb529013ea2e04cd45d519214e79
Neel Smith <dnsmith.neel@gmail.com>
2016-08-19 10:00:12 -0400

    Update for new sig

--- src/test/groovy/edu/holycross/shot/prestochango/TestCiteCollectionObject.groovy
+++ src/test/groovy/edu/holycross/shot/prestochango/TestCiteCollectionObject.groovy
@@ -16,7 +16,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 	 /* Make a collection */
 
 	CiteUrn collUrn = new CiteUrn("urn:cite:testNs:testColl")
-	
+	String descr = "Test collection"
 	CiteUrn prevUrn = new CiteUrn("urn:cite:testNs:testColl.one.v1")
 	CiteUrn nextUrn = new CiteUrn("urn:cite:testNs:testColl.three.v1")
 
@@ -27,13 +27,13 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	ArrayList collProps = [idProp, labelProp, orderedByProp]
 	ArrayList extensions = ["cite:CiteImage","cite:Geo"]
-	
+
 	String orderedProp = "orderedBy"
 	String nsAbbr = "testNs"
 	String nsFull = "http://www.testNs.org/datans"
-	 
-    CiteCollection ccOrdered = new CiteCollection(collUrn, idProp, labelProp, orderedByProp, nsAbbr, nsFull, collProps, extensions)
-    CiteCollection ccUnordered = new CiteCollection(collUrn, idProp, labelProp, null, nsAbbr, nsFull, collProps, extensions)
+
+    CiteCollection ccOrdered = new CiteCollection(collUrn, descr, idProp, labelProp, orderedByProp, nsAbbr, nsFull, collProps, extensions)
+    CiteCollection ccUnordered = new CiteCollection(collUrn,descr, idProp, labelProp, null, nsAbbr, nsFull, collProps, extensions)
 
 
  @Test void testConstructor1() {
@@ -42,7 +42,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.one.v1","label":"object 1","seq":"1"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")
 
 	CiteCollectionObject cco = new CiteCollectionObject(urn,ccUnordered,properties)
 
@@ -54,7 +54,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.one.v1","label":"object 1","seq":"1"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")
 
 	CiteCollectionObject cco = new CiteCollectionObject(urn,ccOrdered,properties,prevUrn,nextUrn)
 
@@ -68,7 +68,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.one.v1","label":"object 1","seq":"1"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")
 
 	CiteCollectionObject cco = new CiteCollectionObject(urn,ccUnordered,properties)
 
@@ -82,14 +82,14 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.one.v1","label":"object 1","seq":"1"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")
 
 	CiteCollectionObject cco = new CiteCollectionObject(urn,ccUnordered,properties)
 
 	assert cco.collection.collProperties.size() == 3
 	assert cco.getPropertyType("urn") == "citeurn"
-	assert cco.getPropertyType("seq") == "number" 
-	assert cco.getPropertyType("label") == "string" 
+	assert cco.getPropertyType("seq") == "number"
+	assert cco.getPropertyType("label") == "string"
  }
 
  @Test void testGetPropertyValue() {
@@ -98,12 +98,12 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.one.v1","label":"object 1","seq":"1"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.one.v1")
 
 	CiteCollectionObject cco = new CiteCollectionObject(urn,ccUnordered,properties)
 
 	assert cco.getPropertyValue("urn").toString() == "urn:cite:testNs:testColl.one.v1"
-	assert cco.getPropertyValue("seq") == 1 
+	assert cco.getPropertyValue("seq") == 1
 	assert cco.getPropertyValue("label") == "object 1"
  }
 
@@ -113,7 +113,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.two.v1","label":"object 2","seq":"2"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")
 	CiteUrn prevUrn = new CiteUrn("urn:cite:testNs:testColl.one.v1")
 	CiteUrn nextUrn = new CiteUrn("urn:cite:testNs:testColl.three.v1")
 
@@ -121,28 +121,28 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	assert cco.urn.toString() == urn.toString()
 	assert cco.getPropertyValue("urn").toString() == "urn:cite:testNs:testColl.two.v1"
-	assert cco.getPropertyValue("seq") == 2 
+	assert cco.getPropertyValue("seq") == 2
 	assert cco.getPropertyValue("label") == "object 2"
-	
+
  }
 
  @Test void testBadProperties1() {
 
 	ArrayList xcollProps = [idProp, labelProp, orderedByProp,booleanProp]
 	ArrayList xextensions = ["cite:CiteImage","cite:Geo"]
-	
+
 	String xorderedProp = "orderedBy"
 	String xnsAbbr = "testNs"
 	String xnsFull = "http://www.testNs.org/datans"
-	 
-    CiteCollection ccTest = new CiteCollection(collUrn, idProp, labelProp, orderedByProp, xnsAbbr, xnsFull, xcollProps, xextensions)
+
+    CiteCollection ccTest = new CiteCollection(collUrn, descr, idProp, labelProp, orderedByProp, xnsAbbr, xnsFull, xcollProps, xextensions)
 	/* Make some property values */
 
 	def properties1 = ["urn":"not-a-urn","label":"object 2","seq":"2","trueOrFalse":"true"]
 	def properties2 = ["urn":"urn:cite:testNs:testColl.two.v1","label":"object 2","seq":"not-a-number","trueOrFalse":"false"]
 	def properties3 = ["urn":"urn:cite:testNs:testColl.two.v1","label":"object 2","seq":"2","trueOrFalse":"notTrueOrFalse"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")
 
 	shouldFail {
 		CiteCollectionObject cco1 = new CiteCollectionObject(urn,ccTest,properties1,prevUrn,nextUrn)
@@ -155,7 +155,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 	shouldFail {
 		CiteCollectionObject cco3 = new CiteCollectionObject(urn,ccTest,properties3,prevUrn,nextUrn)
 	}
-	
+
  }
 
  @Test void testPropertyValues2() {
@@ -167,13 +167,13 @@ class TestCiteCollectionObject extends GroovyTestCase {
 	 String xnsAbbr = "testNs"
 	 String xnsFull = "http://www.testNs.org/datans"
 
-	 CiteCollection ccTest = new CiteCollection(collUrn, idProp, labelProp, orderedByProp, xnsAbbr, xnsFull, xcollProps, xextensions)
+	 CiteCollection ccTest = new CiteCollection(collUrn, descr, idProp, labelProp, orderedByProp, xnsAbbr, xnsFull, xcollProps, xextensions)
 	 /* Make some property values */
 
 	 def properties1 = ["urn":"urn:cite:testNs:testColl.two.v1","label":"object 2","seq":"2","trueOrFalse":"true"]
 	 def properties2 = ["urn":"urn:cite:testNs:testColl.two.v1","label":"object 2","seq":"2","trueOrFalse":"false"]
 
-	 CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")	
+	 CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")
 
 	 CiteCollectionObject cco1 = new CiteCollectionObject(urn,ccTest,properties1,prevUrn,nextUrn)
 	 assert cco1.getPropertyValue("trueOrFalse") == true
@@ -203,7 +203,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.two.v1","label":"object 2","seq":"2"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")
 	CiteUrn prevUrn = new CiteUrn("urn:cite:testNs:testColl.one.v1")
 	CiteUrn nextUrn = new CiteUrn("urn:cite:testNs:testColl.three.v1")
 
@@ -211,7 +211,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	assert cco.urn.toString() == urn.toString()
 	assert cco.getSequence() == 2
-	
+
  }
 
  @Test void testGetSequenceFailsWithUnordered() {
@@ -220,7 +220,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 
 	def properties = ["urn":"urn:cite:testNs:testColl.two.v1","label":"object 2","seq":"2"]
 
-    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")	
+    CiteUrn urn = new CiteUrn("urn:cite:testNs:testColl.two.v1")
 
 		CiteCollectionObject cco = new CiteCollectionObject(urn,ccUnordered,properties)
 
@@ -228,8 +228,7 @@ class TestCiteCollectionObject extends GroovyTestCase {
 	shouldFail{
 		assert cco.getSequence() == 2
 	}
-	
+
  }
 
 }
-
