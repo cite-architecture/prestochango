01564e768102011a98981f3dbc06f48b572dee48
Neel Smith <dnsmith.neel@gmail.com>
2016-08-19 10:26:03 -0400

    Include actual implementation

--- src/main/groovy/edu/holycross/shot/prestochango/LocalFileImplementation.groovy
+++ src/main/groovy/edu/holycross/shot/prestochango/LocalFileImplementation.groovy
@@ -1,27 +1,54 @@
 package edu.holycross.shot.prestochango
 
 import edu.holycross.shot.safecsv.SafeCsvReader
+import org.apache.commons.io.FilenameUtils
 
 class LocalFileImplementation implements CollectionImplementation {
 
     File delimitedFile
     ImplementationType implType
 
+    String fileExtension
 
     LocalFileImplementation(File f) {
       delimitedFile = f
       implType = ImplementationType.LOCAL_FILE
     }
 
-    ArrayList getRecordArray(boolean asUnicode) {
-      def records = []
-      delimitedFile.eachLine {
-
+    /** Reads delimited file and returns an array of records,
+    * including initial row of strings with identifying property labels.
+    */
+    ArrayList getRecordArray() {
+      switch (FilenameUtils.getExtension(delimitedFile.toString())) {
+      case "csv":
+      return getCsvAsArray()
+      break
+      
+      case "tsv":
+      return getTabAsArray()
+      break
+      
+      default:
+      throw new Exception("Unsupported filename extension in " + delimitedString)
+      break
       }
-      return delimitedFile.getText()
+
    }
 
+   ArrayList getCsvAsArray() {
+     SafeCsvReader srcReader = new SafeCsvReader(delimitedFile)
+     return srcReader.readAll()
+   }
 
+  ArrayList getTabAsArray() {
+    def records = []
+    delimitedFile.eachLine {
+      records.add(it.split(/\t/))
+    }
+    return records
+   }
+
+  
    String toString() {
      return "Collection implemented in local file " + delimitedFile
    }
