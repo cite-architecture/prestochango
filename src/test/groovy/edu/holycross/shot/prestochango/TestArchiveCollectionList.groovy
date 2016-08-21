afdc70ad4bc011c5850671d842705cd1b9d3c9e5
Neel Smith <dnsmith.neel@gmail.com>
2016-08-21 14:36:14 -0400

    Test collections

--- src/test/groovy/edu/holycross/shot/prestochango/TestArchiveCollectionList.groovy
+++ src/test/groovy/edu/holycross/shot/prestochango/TestArchiveCollectionList.groovy
@@ -8,23 +8,43 @@ import org.junit.Test
 
 
 
-/** Class testing output of ttl from prestochango's CollectionArchive class.
+/** Class testing prestochange CiteCollection class.
 */
 class TestArchiveCollectionList extends GroovyTestCase {
-  String inventoryName = "testdata/signs-collection.xml"
-  File inv = new File(inventoryName)
+
+
   String schemaFileName = "schemas/CiteCollectionInventory.rng"
 
- // Ordered colections MUST have a sequence value
- @Test void testConstructor() {
-  CollectionArchive cca = new CollectionArchive(inv, schemaFileName, new File("/dev/null"))
-  CiteUrn urn = new CiteUrn("urn:cite:hmt:critsigns")
 
-  def collList = cca.getCollections()
-  assert collList.size() == 1
-  assert collList[0].urn.toString() ==   "urn:cite:hmt:critsigns"
+  @Test void testSingle() {
+    String inventoryName = "testdata/signs-collection.xml"
+    File inv = new File(inventoryName)
+    CollectionArchive cca = new CollectionArchive(inv, schemaFileName, new File("/dev/null"))
+    CiteUrn urn = new CiteUrn("urn:cite:hmt:critsigns")
+
+    def collList = cca.getCollections()
+    assert collList.size() == 1
+    assert collList[0].urn.toString() ==   "urn:cite:hmt:critsigns"
+  }
 
 
- }
 
+  @Test void testMultiple() {
+    String testInventory = "testdata/collections.xml"
+    File inv = new File(testInventory)
+
+    String dataDir = "testdata/csvs"
+    File dir = new File(dataDir)
+    CollectionArchive cca = new CollectionArchive(inv, schemaFileName, dir)
+    def expectedNumber = 3
+    assert cca.getCollections().size() == expectedNumber
+
+    def expectedUrns = ["urn:cite:hmt:vaimg","urn:cite:hmt:critsigns","urn:cite:hmt:msA"]
+    def actualUrns = []
+    cca.getCollections().each {
+      actualUrns.add(it.urn.toString())
+    }
+    assert actualUrns == expectedUrns
+
+  }
 }
